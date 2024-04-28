832a443e79b4291b0ac2f370c30c88cc8c543d37
OSCAR MUNOZ <omunozoyarce@uc.cl>
2024-04-27 22:29:36 -0400

    colored item names

--- villager_trade.py
+++ villager_trade.py
@@ -1,28 +1,56 @@
 import pyperclip
+from pprint import pprint
 
 class Item():
-    def __init__(self, id: str, name: str = None, enchantments: list = []):
+    def __init__(self, id: str, name: str = None, color : str = None, enchantments: list = []):
         self.id = id 
-        self.name = f"display:{{Name:'{{\"text\":\"{name}\"}}'}}" if name else None        
+        
+        if name and color:
+            self.name = f"display:{{Name:'{{\"text\":\"{name}\", \"color\":\"{color}\"}}'}}"
+        elif name and not color:
+            self.name = f"display:{{Name:'{{\"text\":\"{name}\"}}'}}" if name else None        
+        else:
+            self.name = None
+        
+        
         self.enchantments = enchantments
-        self.enchantments = f"Enchantments:[{','.join(str(enchantment) for enchantment in self.enchantments)}]"
-    def __str__(self):
-        if self.enchantments == "Enchantments:[]":
-            return f"give @p {self.id}{{{self.name}}}"
-        return f"give @p {self.id}{{{self.name},{self.enchantments}}}"
-
-class VillagerTrade():
-    def __init__(self, buy: Item = None, buy_B: Item = None, sell: Item = None, reward_exp: int = 0, max_uses: int = 9999999):
-        self.buy = buy if buy else None
-        self.buyB = buy_B if buy_B else None
-        self.sell = sell if sell else None
-        self.rewardExp = reward_exp
-        self.maxUses = max_uses
+        if enchantments != []:
+            if self.id == "minecraft:enchanted_book":
+                if type(self.enchantments) == Enchantment:
+                    self.enchantments = f"StoredEnchantments:[{self.enchantments}]"
+                else:
+                    self.enchantments = f"StoredEnchantments:[{','.join(str(enchantment) for enchantment in self.enchantments)}]"
+            else:
+                if type(self.enchantments) == Enchantment:
+                    self.enchantments = f"Enchantments:[{self.enchantments}]"
+                else:
+                    self.enchantments = f"Enchantments:[{','.join(str(enchantment) for enchantment in self.enchantments)}]"
     
+    def give_item(self):
+        if self.enchantments == [] and self.name != None:
+            return f"give @p {self.id}{{{self.name}}}"
+        elif self.enchantments != [] and self.name == None:
+            return f"give @p {self.id}{{{self.enchantments}}}"
+        elif self.enchantments == [] and self.name == None:
+            return f"give @p {self.id}"
+        return f"give @p {self.id}{{{self.name}, {self.enchantments}}}"
+    
+    def __repr__(self):
+        if self.enchantments == [] and self.name != None:
+            return f"{{id:{self.id},name:{self.name}}}"
+        elif self.enchantments != [] and self.name == None:
+            return f"{{id:{self.id},{self.enchantments}}}"
+        elif self.enchantments == [] and self.name == None:
+            return f"{{id:{self.id}}}"
+        return f"{{id:{self.id},name:{self.name},{self.enchantments}}}"
+
+
+class Enchantment(): 
+    def __init__(self, id: str, level: int = 1):
+        self.id = id
+        self.level = level
     def __str__(self):
-        return str((self.buy, self.buyB, self.sell, self.rewardExp, self.maxUses))
-
-
+        return f'{{lvl:{self.level}s, id:"{self.id}"}}'
 
 
 
@@ -45,22 +73,26 @@ class VillagerTrade():
 #                             specialPrice: 0, 
 #                             demand: 0, 
 #                             rewardExp: 1b}]}
-
-class Enchantment():
-    def __init__(self, id: str, level: int):
-        self.id = id
-        self.level = level
+class VillagerTrade():
+    def __init__(self, buy: Item = None, buy_B: Item = None, sell: Item = None, reward_exp: int = 0, max_uses: int = 9999999):
+        self.buy = buy if buy else None
+        self.buyB = buy_B if buy_B else None
+        self.sell = sell if sell else None
+        self.rewardExp = reward_exp
+        self.maxUses = max_uses
+    
     def __str__(self):
-        return f'{{lvl: {self.level}s, id: "{self.id}"}}'
-enchantments = [
-    Enchantment("minecraft:protection", 3),
-    Enchantment("minecraft:aqua_affinity", 1)
-]
+        return str(self.buy)
 
-item_1 = Item("minecraft:diamond_helmet", "Helm. Prot")
-item_2 = Item("minecraft:enchanted_book", "Helm. Prot", enchantments)
-print(item_1)
-print(item_2)
 
-trade = VillagerTrade(item_1, item_2, 1, 0)
-print(trade)
+
+
+item = Item("minecraft:villager_spawn_egg", "Fart", "blue")
+pyperclip.copy(item.give_item())
+print(item.give_item())
+# give @p minecraft:acacia_boat{display:{Name:'{"text":"Tunic of Destiny", "color":"blue"}'}} this one works
+# give @p minecraft:acacia_boat{display:{Name:'{"text":"Tunic of Destiny", "color":"blue"}',Lore:['{"text":"Fart"}']}} 
+# give @p minecraft:acacia_boat{display:{Name:'{"text":"Tunic of Destiny","color":"blue"}
+
+# give @p minecraft:acacia_boat{display:{Name:'{"text":"My love for my wife Yvain <3"}, color:16777215}'}
+# give @p minecraft:acacia_boat{display:{Name:'{"text":"My love for my wife Yvain <3", color:16777215}'}}
