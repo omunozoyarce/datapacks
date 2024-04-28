2ec02f3eb2a9d4271b7adadf7635d86120e9eab0
omunozoyarce <omunozoyarce@uc.cl>
2024-04-28 15:36:48 -0400

    villager system complete

--- villager.py
+++ villager.py
@@ -1,28 +1,62 @@
-# give @p villager_spawn_egg nbt:
-# {EntityTag:{VillagerData:{type:plains,profession:farmer,level:2},Offers:{Recipes:[{maxUses:1,buy:{id:acacia_button,Count:1},buyB:{id:acacia_door,Count:1}}]}}} 1
 import pyperclip
+from villager_trade import VillagerTrade
+from random import randint
 
 class Villager():
+    
 
-    def __init__(self, name: str, type: str, profession: str, level: int, persistent: bool = True, trades: list = []):
-        self.name = f"CustomName:'[{{\"text\":\"{name}\"}}]'" if name else None
-        self.type = type if type else "plains"
-        self.profession = profession if profession else "farmer"
-        self.level = level if level else 1
+    def __init__(self, name: str, color: str = None, type: str = None, profession: str = None, level: int = None, persistent: bool = True, trades: list = [], ai: bool = True):
+        vanilla_colors = ("dark_red, red, gold, yellow, dark_green, green, aqua, dark_aqua, dark_blue, blue, light_purple, dark_purple, white, gray, dark_gray, black").split(", ")
+        if color:
+            if color in vanilla_colors or color == "random":
+                if color == "random":
+                    color = vanilla_colors[randint(0, 15)]
+                self.name = f"CustomName:'{{\"text\":\"{name}\", \"color\":\"{color}\"}}'"
+            else:
+                self.name = f"CustomName:'{{\"text\":\"{name}\"}}'"
+        elif name:
+            self.name = f"CustomName:'{{\"text\":\"{name}\"}}'"
+        else:
+            self.name = None
+        self.type = type if type != None else "plains"
+        self.profession = profession if profession != None else "farmer"
+        self.level = level if level != None else 0
         ''' Vanilla type table:
         desert, jungle, plains, savanna, snow, swamp, taiga'''
         
         ''' Vanilla profession table:
         armorer, butcher, cartographer, cleric, farmer, 
         fisherman, fletcher, leatherworker, librarian, 
-        mason, nitwit, shepherd, toolsmith, weaponsmith
-        '''
+        mason, nitwit, shepherd, toolsmith, weaponsmith'''
+        
+        ''' Vanilla color table:
+        dark_red, red, gold, yellow, dark_green, green,
+        aqua, dark_aqua, dark_blue, blue, light_purple,
+        dark_purple, white, gray, dark_gray, black'''
         self.persistent = "PersistenceRequired:1" if persistent else "PersistenceRequired:0"
         self.trades = trades
         self.villager_data = f"VillagerData:{{type:{self.type},profession:{self.profession},level:{self.level}}}"
+        self.ai = "NoAI:1b" if not ai else ""
     
     def give_spawn_egg(self):
-        return f"give @p villager_spawn_egg{{EntityTag:{{{self.name},{self.villager_data},{self.persistent}}}}} 1"
+        if self.name:
+            if self.ai:
+                return f"give @p minecraft:villager_spawn_egg{{EntityTag:{{{self.name},{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}},{self.ai}}}}} 1"
+            return f"give @p minecraft:villager_spawn_egg{{EntityTag:{{{self.name},{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}}}}}} 1"
+        if self.ai:
+            return f"give @p minecraft:villager_spawn_egg{{EntityTag:{{{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}},{self.ai}}}}} 1"
+        return f"give @p minecraft:villager_spawn_egg{{EntityTag:{{{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}}}}}} 1"
+    
+    def summon(self):
+        if self.name:
+            if self.ai:
+                return f"summon minecraft:villager ~ ~1 ~ {{{self.name},{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}},{self.ai}}}"
+            return f"summon minecraft:villager ~ ~1 ~ {{{self.name},{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}}}}"
+        if self.ai:
+            return f"summon minecraft:villager ~ ~1 ~ {{{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}},{self.ai}}}"
+        return f"summon minecraft:villager ~ ~1 ~ {{{self.villager_data},{self.persistent},Offers:{{Recipes:{self.trades}}}}}"
+
+
 
     def __str__(self):
         return f'''{{profession:{self.profession},
@@ -35,8 +69,8 @@ Offers:{{Recipes:[{self.offers}]}}}}
 
 
 if __name__ == "__main__":
-    villager = Villager("bob", "jungle", "fisherman", 2)
-    print(villager.give_spawn_egg())
-    
-    pyperclip.copy(villager.give_spawn_egg())
-
+    trades = [VillagerTrade(("diamond_hoe"), ("tnt"), ("ice")), VillagerTrade(("bow"),None, ("arrow"))]
+    villager = Villager("bob", "cafa", level=2, persistent=True, trades=trades, ai=False)
+    egg = (villager.summon())
+    print(egg)
+    pyperclip.copy(egg)
--- villager_trade.py
+++ villager_trade.py
@@ -52,36 +52,6 @@ class Item():
         if self.enchantments == [] and self.name == None:
             return f"{{id:\"{self.id}\",Count:{count}}}"
         return f"{{id:\"{self.id}\",Count:{count},tag:{{{self.name}, {self.enchantments}}}}}"
-        
-# {id:"bookshelf",Count:1,tag:{display:{Name:'{"text":"Fart", "color":"blue"}'}}}
-
-# /give @p villager_spawn_egg
-# {EntityTag:
-# {VillagerData:
-# {type:plains,profession:farmer,level:2},
-# 
-# Offers:{Recipes:[{buy:{id:book,Count:1},sell:{id:emerald,Count:1}}]},CustomName:'[{"text":"bob"}]'}} 1
-
-# Offers: 
-#                     {Recipes: 
-#                         [{maxUses: 2147483647, 
-#                             buyB: {id: "minecraft:emerald", Count: 16b}, 
-#                             buy: {id: "minecraft:diamond_helmet", Count: 1b, tag: {Damage: 0}}, 
-#                             sell: {id: "minecraft:enchanted_book", Count: 1b, tag: {RepairCost: 7, 
-#                             StoredEnchantments: 
-#                                 [{lvl: 3s, id: "minecraft:respiration"}, 
-#                                 {lvl: 1s, id: "minecraft:aqua_affinity"}, 
-#                                 {lvl: 4s, id: "minecraft:protection"}, 
-#                                 {lvl: 3s, id: "minecraft:unbreaking"}, 
-#                                 {lvl: 1s, id: "minecraft:mending"}], 
-#                             display: {Name: '{"text":"Helm. Prot."}'}}}, 
-#                             xp: 1, 
-#                             uses: 0, 
-#                             priceMultiplier: 0.0f, 
-#                             specialPrice: 0, 
-#                             demand: 0, 
-#                             rewardExp: 1b}]}
-
 
 class Enchantment(): 
     def __init__(self, id: str, level: int = 1):
@@ -93,29 +63,44 @@ class Enchantment():
 
 
 class VillagerTrade():
-    def __init__(self, buy: Item = None, buy_B: Item = None, sell: Item = None, reward_exp: int = 0, max_uses: int = 9999999):
-        self.buy = buy if buy else None
-        self.buyB = buy_B if buy_B else None
-        self.sell = sell if sell else None
+    def __init__(self, buy: tuple = None, buy_B: tuple = None, sell: tuple = None, reward_exp: int = 0, max_uses: int = 9999999):
         self.rewardExp = reward_exp
         self.maxUses = max_uses
-    
-    def give_spawn_egg(self):
-        return
-
-
-
-
-item = Item("minecraft:diamond_hoe", "Fart", "blue", [Enchantment("minecraft:sharpness", 1), Enchantment("minecraft:unbreaking", 2)])
-pyperclip.copy(item.tag())
-print(item.tag())
-
-# give @p minecraft:villager_spawn_egg{display:{Name:'{"text":"Fart", "color":"blue"}'}}
-# /give @p villager_spawn_egg{EntityTag:{VillagerData:{type:plains,profession:farmer,level:2},Offers:{Recipes:[{buy:{id:"minecraft:diamond_hoe",Count:1,tag:{display:{Name:'{"text":"Fart", "color":"blue"}'}, Enchantments:[{lvl:1s, id:"minecraft:sharpness"},{lvl:2s, id:"minecraft:unbreaking"}]}},sell:{id:emerald,Count:1}}]},CustomName:'[{"text":"bob"}]'}} 1
-
-# give @p minecraft:acacia_boat{display:{Name:'{"text":"Tunic of Destiny", "color":"blue"}'}} this one works
-# give @p minecraft:acacia_boat{display:{Name:'{"text":"Tunic of Destiny", "color":"blue"}',Lore:['{"text":"Fart"}']}} 
-# give @p minecraft:acacia_boat{display:{Name:'{"text":"Tunic of Destiny","color":"blue"}
-
-# give @p minecraft:acacia_boat{display:{Name:'{"text":"My love for my wife Yvain <3"}, color:16777215}'}
-# give @p minecraft:acacia_boat{display:{Name:'{"text":"My love for my wife Yvain <3", color:16777215}'}}
+        
+        if type(buy) == tuple:
+            if type(buy[0]) == Item:
+                self.buy = buy[0].tag(buy[1])
+            elif type(buy[0]) == str:
+                self.buy = f"{{id:{buy[0]},Count:{buy[1]}}}"
+        
+        elif type(buy) == str:
+            self.buy = f"{{id:{buy},Count:1}}"
+        
+        elif type(buy) == Item:
+            self.buy = buy.tag()
+        
+        if buy_B:
+            if type(buy_B) == tuple:
+                if type(buy_B[0]) == Item:
+                    self.buyB = buy_B[0].tag(buy_B[1])
+                elif type(buy_B[0]) == str:
+                    self.buyB = f"{{id:{buy_B[0]},Count:{buy_B[1]}}}"
+            elif type(buy_B) == str:
+                self.buyB = f"{{id:{buy_B},Count:1}}"
+        else:
+            self.buyB = None
+        
+        if type(sell) == tuple:
+            if type(sell[0]) == Item:
+                self.sell = sell[0].tag(sell[1])
+            elif type(sell[0]) == str:
+                self.sell = f"{{id:{sell[0]},Count:{sell[1]}}}"
+        elif type(sell) == str:
+            self.sell = f"{{id:{sell},Count:1}}"
+        elif type(sell) == Item:
+            self.sell = sell.tag()
+        
+    def __repr__(self):
+        if self.buyB == None:
+            return f"{{buy:{self.buy},sell:{self.sell},rewardExp:{self.rewardExp}b,maxUses:{self.maxUses}}}"
+        return f"{{buy:{self.buy},buyB:{self.buyB},sell:{self.sell},rewardExp:{self.rewardExp}b,maxUses:{self.maxUses}}}"
