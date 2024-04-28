0f3dc7827ef3d93b9abc729a581a3d20ce3c0080
OSCAR MUNOZ <omunozoyarce@uc.cl>
2024-04-28 18:34:59 -0400

    finished

--- villager.py
+++ villager.py
@@ -1,11 +1,12 @@
 import pyperclip
-from villager_trade import VillagerTrade
+from villager_trade import VillagerTrade, Item, Enchantment
 from random import randint
 
 class Villager():
     
 
-    def __init__(self, name: str, color: str = None, type: str = None, profession: str = None, level: int = None, persistent: bool = True, trades: list = [], ai: bool = True):
+    def __init__(self, name: str, color: str = None, type: str = None, profession: str = None, 
+                 level: int = None, persistent: bool = True, trades: list = [], ai: bool = True):
         vanilla_colors = ("dark_red, red, gold, yellow, dark_green, green, aqua, dark_aqua, dark_blue, blue, light_purple, dark_purple, white, gray, dark_gray, black").split(", ")
         if color:
             if color in vanilla_colors or color == "random":
@@ -19,7 +20,7 @@ class Villager():
         else:
             self.name = None
         self.type = type if type != None else "plains"
-        self.profession = profession if profession != None else "farmer"
+        self.profession = profession if profession != None else None
         self.level = level if level != None else 0
         ''' Vanilla type table:
         desert, jungle, plains, savanna, snow, swamp, taiga'''
@@ -35,7 +36,10 @@ class Villager():
         dark_purple, white, gray, dark_gray, black'''
         self.persistent = "PersistenceRequired:1" if persistent else "PersistenceRequired:0"
         self.trades = trades
-        self.villager_data = f"VillagerData:{{type:{self.type},profession:{self.profession},level:{self.level}}}"
+        if self.profession != None:
+            self.villager_data = f"VillagerData:{{type:{self.type},profession:{self.profession},level:{self.level}}}"
+        else:
+            self.villager_data = f"VillagerData:{{type:{self.type}}}"
         self.ai = "NoAI:1b" if not ai else ""
     
     def give_spawn_egg(self):
@@ -69,8 +73,11 @@ Offers:{{Recipes:[{self.offers}]}}}}
 
 
 if __name__ == "__main__":
-    trades = [VillagerTrade(("diamond_hoe"), ("tnt"), ("ice")), VillagerTrade(("bow"),None, ("arrow"))]
-    villager = Villager("bob", "cafa", level=2, persistent=True, trades=trades, ai=False)
-    egg = (villager.summon())
-    print(egg)
-    pyperclip.copy(egg)
+    # enchanted_book = Item("minecraft:enchanted_book", name="sex 2",  enchantments = [Enchantment("minecraft:sharpness", 5), Enchantment("minecraft:unbreaking", 3),
+    #                                                                   Enchantment("minecraft:looting", 3), Enchantment("minecraft:fire_aspect", 2),
+    #                                                                   Enchantment("minecraft:knockback", 2), Enchantment("minecraft:mending", 1)])
+    # trade = VillagerTrade("emerald", None, enchanted_book)
+    villager = Villager("Papa Meat", "red", "plains", "armorer", None, True, None, False)
+    pyperclip.copy(villager.summon())
+    
+    # summon minecraft:villager ~ ~1 ~ {CustomName:'{"text":"Papa Meat", "color":"red"}',PersistenceRequired:1,Offers:{Recipes:None},NoAI:1b}
